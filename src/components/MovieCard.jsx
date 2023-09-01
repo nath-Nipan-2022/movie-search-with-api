24e1ae7d163e11f0c5137d2f3781804fd905388a
Nipan Debnath <debnathnipan719@gmail.com>
2023-09-01 23:51:36 +0530

    style the movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import placeholder from "../assets/placeholder-image.png";
+import placeholder from "../assets/images/placeholder-image.png";
+import { starIcon, plusIcon } from "../assets/icons";
+import { Link } from "react-router-dom";
 
 const MovieCard = ({ show: movie }) => {
   const { image, name, premiered, rating, genres } = movie;
 
   return (
-    <a className="movie_card" href={movie.id}>
-      <figure>
+    <Link className="movie_card" to={`/movie/${movie.id}`}>
+      <figure className="movie-image">
         <img
           src={image ? image.medium : placeholder}
           alt="movie poster"
@@ -14,23 +16,28 @@ const MovieCard = ({ show: movie }) => {
         />
       </figure>
 
-      <article className="movie_desc">
-        <div className="details">
-          <h3 className="genre">
-            <span>{genres[0]}</span>
-          </h3>
-          <h3 className="rating">
-            <span>{rating.average != null ? rating.average + "/10" : ""}</span>
-          </h3>
-          <h3 className="year">
-            <span>{premiered} </span>
-          </h3>
+      <article className="movie_desc flex justify-between">
+        <div className="movie_desc_rating flex align-center">
+          <img src={starIcon} alt="star icon" width={12} height={12} />
+          <span>{rating.average != null ? rating.average : ""}</span>
+        </div>
+        {/* title & desc */}
+        <div className="movie_desc_main">
           <h3 className="title">
             <span>{name}</span>
           </h3>
+          <p className="year">
+            <span>{premiered.slice(0, 4)} </span>
+          </p>
+          <div className="btns flex align-center justify-between">
+            <button className="primary-btn">Watch Now</button>
+            <button className="plus-btn">
+              <img src={plusIcon} alt="plus icon" width={12} height={12} />
+            </button>
+          </div>
         </div>
       </article>
-    </a>
+    </Link>
   );
 };
 
